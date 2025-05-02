40d043da507d2dee5b327a667d5c841b939530de
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2025-05-02 17:35:39 -0500

    Set Quantity in Contains to non-null and default 1

--- CREATE_TABLE_s/Contains.sql
+++ CREATE_TABLE_s/Contains.sql
@@ -3,7 +3,7 @@ CREATE TABLE Contains (
     Oid INT NOT NULL,
     Bid INT NOT NULL,
     Price DECIMAL(10,2),
-    Quantity INT,
+    Quantity INT NOT NULL DEFAULT 1,
     PRIMARY KEY (Oid, Bid),
     FOREIGN KEY (Oid) REFERENCES Orders(Oid),
     FOREIGN KEY (Bid) REFERENCES BookInventory(Bid)
