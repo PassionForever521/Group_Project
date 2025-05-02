fa7a79efa1af06ee787ebbc53d85bd0bb00e72e5
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2025-05-02 17:34:57 -0500

    Add ShipAddr-related fields to Orders table

--- CREATE_TABLE_s/Orders.sql
+++ CREATE_TABLE_s/Orders.sql
@@ -4,6 +4,11 @@ CREATE TABLE Orders (
     ShipDate DATE,
     OrderDate DATE NOT NULL,
     Status VARCHAR(255) NOT NULL,
+    -- ShipStreet through ShipZip map to the ShipAddr compound attribute from the relational schema.
+    ShipStreet VARCHAR(255) NOT NULL,
+    ShipCity VARCHAR(255) NOT NULL,
+    ShipState VARCHAR(255) NOT NULL,
+    ShipZip VARCHAR(10) NOT NULL,
     Cid INT NOT NULL,
     Pid INT NOT NULL,
     Eid INT NOT NULL,
