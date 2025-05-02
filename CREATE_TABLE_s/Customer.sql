c8873ac32dd930733ff5910c110144e9f6302719
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2025-05-02 17:33:10 -0500

    Add missing Zip column to Customer field

--- CREATE_TABLE_s/Customer.sql
+++ CREATE_TABLE_s/Customer.sql
@@ -4,6 +4,7 @@ CREATE TABLE Customer (
     Street VARCHAR(255),
     City VARCHAR(255),
     State VARCHAR(255),
+    Zip VARCHAR(10), -- To accomodate 5-digit (e.g. 12345) as well as 9-digit (e.g. 12345-6789) codes
     Cfirstname VARCHAR(255) NOT NULL,
     Clastname VARCHAR(255) NOT NULL,
     Email VARCHAR(255) UNIQUE,
